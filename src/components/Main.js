49c89cd420319a240da836cec9fc75206c18164c
Ban-biaysme <biyas.banerjee2013@gmail.com>
2021-07-22 15:12:32 +1200

    components added for headermfooter and main

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Hotelinfo from './Hotelinfo';
+import Welcome from './Welcome';
+
+
+function Main() {
+    return (
+            <main id="wrapper">
+            <Welcome />
+            <Hotelinfo />
+          </main>
+        )
+    }
+
+export default Main;
